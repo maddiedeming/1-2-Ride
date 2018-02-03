65443e4f7338014279367a10a6b6ab3434705163
Alex Harris <akharri8@ncsu.edu>
2018-02-02 20:22:19 -0500

    Added responsive starter charts

--- assets/javascript/charts.js
+++ assets/javascript/charts.js
@@ -8,11 +8,19 @@ const textLineChart = new Chart(lineChart, {
         labels: ["Mon", "Tues", "Wed", "Thurs", "Frid", "Sat", "Sun"],
         datasets: [{
             label: "Placeholder Data Set",
-            backgroundColor: 'rgb(255, 255, 255)',
+            fill: false,
+            lineTenstion: 0,
             borderColor: 'rgb(255, 255, 255)',
             data: [0, 10, 5, 2, 20, 30, 45],
-        }]
-    }
+        },
+        {
+            label: "2nd Data Set",
+            fill: false,
+            backgroundColor: 'rgb(0, 255, 0)',
+            borderColor: 'rgb(0, 0, 255)',
+            data: [0, 5, 25, 25, 20, 30, 35],
+        }       
+        ]}
 });
 
 
@@ -22,9 +30,11 @@ const textLineChart = new Chart(lineChart, {
 const doughnutChart = document.getElementById("dough-chart");
 const textDoughnutChart = new Chart(doughnutChart, {
     type: 'doughnut',
-    data: data = {
+    data: {
         datasets: [{
-
+            label: "red",
+            fill: true,
+            backgroundColor: "rgb(250, 0, 0)",
             data: [10, 20, 30]
         }],
     
