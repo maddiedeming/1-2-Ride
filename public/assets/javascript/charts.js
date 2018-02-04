5fc75aa72e3c4652992501c894c7814d6a4d7e0d
Alex Harris <akharri8@ncsu.edu>
2018-02-04 00:00:34 -0500

    Integrated both datasets into the same chartgit add .git add .

--- public/assets/javascript/charts.js
+++ public/assets/javascript/charts.js
@@ -1,67 +1,157 @@
 
-// Example Line Chart
+// // Example Line Chart
 
-function apples(response){
-    console.log("Got response in chart function");
-      return function chartTest(response) {
-          console.log(response);
-          let labels = [];
-          for(var i = 0; i < response.cost_estimates.length; i++) {
-              labels.push("Driver " + (i + 1));
-          }
-          console.log(labels);
+// function apples(response){
+//     console.log("Got response in chart function");
+//       return function chartTest(response) {
+//           console.log(response);
+//         // Each API call will need a different if statment to determine which API 
+//         // we are currently using
+//           if("cost_estimates" in response === true) {
+//             var labels = [];
+//             for(var i = 0; i < response.cost_estimates.length; i++) {
+//               labels.push("Driver " + (i + 1));
+//             }
+//             console.log("labels: " + labels);
   
-          let costData = [];
-          for(var i = 0; i < response.cost_estimates.length; i++) {
-              costData.push(response.cost_estimates[i].estimated_cost_cents_max);
-          }
-          console.log(costData);
-  
-          const lineChart = document.getElementById('line-chart').getContext('2d');
-          const textLineChart = new Chart(lineChart, {
-              type: 'line',
-              data: {
-                  labels: labels,
-                  datasets: [{
-                      label: "Placeholder Data Set",
-                      fill: false,
-                      lineTenstion: 0,
-                      borderColor: 'rgb(255, 255, 255)',
-                      data: costData,
-                  }
-                  // {
-                  //     label: "2nd Data Set",
-                  //     fill: false,
-                  //     backgroundColor: 'rgb(0, 255, 0)',
-                  //     borderColor: 'rgb(0, 0, 255)',
-                  //     data: [0, 5, 25, 25, 20, 30, 35],
-                  // }       
-                  ]}
-          });
-          console.log(textLineChart);
-      }
-  }
+//             var costData = [];
+//             for(var i = 0; i < response.cost_estimates.length; i++) {
+//                 costData.push(response.cost_estimates[i].estimated_cost_cents_max);
+//             }
+//             console.log("Cost Data: " + costData);
+//             var costDataLyft = costData;
+//             var lyftDataSet = {
+//                 label: "Lyft",
+//                 fill: false,
+//                 lineTenstion: 0,
+//                 borderColor: 'rgb(255, 255, 255)',
+//                 data: costDataLyft
+//             }
+//             console.log(lyftDataSet);
+//           } else if("prices" in response === true) {
+//                 var costDataUber = [];
+//                 for(var i = 0; i < response.prices.length; i++) {
+//                     costDataUber.push(response.prices[i].high_estimate);
+//                 }
+
+//                 var uberDataSet = {         
+//                     label: "Uber",
+//                     fill: false,
+//                     backgroundColor: 'rgb(0, 255, 0)',
+//                     borderColor: 'rgb(0, 0, 255)',
+//                     data: costDataUber
+//                 }   
+//                 console.log(uberDataSet);
+//           }
+
+//           console.log(costData);
+//           const lineChart = document.getElementById('line-chart').getContext('2d');
+//           const textLineChart = new Chart(lineChart, {
+//               type: 'line',
+//               data: {
+//                   labels: labels,
+//                   datasets: [ true ? lyftDataSet : uberDataSet, 
+//                     false ? lyftDataSet : uberDataSet
+//                   ]}
+//           });
+//           console.log(textLineChart);
+//       }
+// }
+
+
+function lyftLineChart(response) {
+    console.log(response);
+    var labels = [];
+    for(var i = 0; i < response.cost_estimates.length; i++) {
+        labels.push("Driver " + (i + 1) + " ");
+    }
+    console.log("labels: " + labels);
+    var costData = [];
+    for(var i = 0; i < response.cost_estimates.length; i++) {
+        costData.push(response.cost_estimates[i].estimated_cost_cents_max);
+    }
+    console.log("Cost Data: " + costData);
+    var lyftCostData = costData;
+    var lyftDataSet = {
+        label: 'Lyft',
+        fill: false,
+        lineTenstion: 0,
+        borderColor: 'rgb(255, 255, 255)',
+        data: lyftCostData
+    };
+    var lyftData = [lyftDataSet, labels]
+    return lyftData;
+}  
+
+
+
+function uberLineChart(response) {
+    console.log(response);
+    var costData = [];
+    for(var i = 0; i < response.prices.length; i++) {
+        costData.push(response.prices[i].high_estimate);
+    }
+    console.log("Cost Data: " + uberCostData);
+    var uberCostData = costData;
+    var uberDataSet = {
+        label: 'Uber',
+        fill: false,
+        lineTenstion: 0,
+        borderColor: 'rgb(0, 255, 0)',
+        data: uberCostData
+    };
+    var uberData = uberDataSet;
+    return uberData;
+}
+
+
+
+// Draws Chart
+function LineChartRender(lyftLabels, lyftDataSet, uberData){
+    console.log("Labels: " + lyftLabels);
+    console.log("DataSet: " + lyftDataSet);
+    const lineChart = document.getElementById('line-chart').getContext('2d');
+    const textLineChart = new Chart(lineChart, {
+        type: 'line',
+        data: {
+            labels: lyftLabels,
+            datasets: [lyftDataSet, uberData] 
+            }
+    });
+    console.log(textLineChart);
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 // Example Doughnut Chart
 
-const doughnutChart = document.getElementById("dough-chart");
-const textDoughnutChart = new Chart(doughnutChart, {
-    type: 'doughnut',
-    data: {
-        datasets: [{
-            label: "red",
-            fill: true,
-            backgroundColor: "rgb(250, 0, 0)",
-            data: [10, 25, 35]
-        }],
+// const doughnutChart = document.getElementById("dough-chart");
+// const textDoughnutChart = new Chart(doughnutChart, {
+//     type: 'doughnut',
+//     data: {
+//         datasets: [{
+//             label: "red",
+//             fill: true,
+//             backgroundColor: "rgb(250, 0, 0)",
+//             data: [10, 25, 35]
+//         }],
     
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: [
-            'Bananas',
-            'Yellow',
-            'Blue'
-        ]
-    },
-});
+//         // These labels appear in the legend and in the tooltips when hovering different arcs
+//         labels: [
+//             'Bananas',
+//             'Yellow',
+//             'Blue'
+//         ]
+//     },
+// });
--- public/assets/javascript/scripts.js
+++ public/assets/javascript/scripts.js
@@ -52,100 +52,107 @@ function parseLyftData(data){
   
 }
 function submitInfo(){
-event.preventDefault();
-let address = replaceSpaces($("#address").val());
-let city = replaceSpaces($("#city").val().trim());
-let state = $("#state").val().trim();
-let lat = 0;
-let lng = 0;
-let destAddress = replaceSpaces($("#destAddress").val());
-let destCity = replaceSpaces($("#destCity").val());
-let destState = $("#destState").val();
+  event.preventDefault();
+  let address = replaceSpaces($("#address").val());
+  let city = replaceSpaces($("#city").val().trim());
+  let state = $("#state").val().trim();
+  let lat = 0;
+  let lng = 0;
+  let destAddress = replaceSpaces($("#destAddress").val());
+  let destCity = replaceSpaces($("#destCity").val());
+  let destState = $("#destState").val();
 
 
 //Lyft Begins
-$.ajax({url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + ',+' + city + ',+' + state + '&key=AIzaSyC8RAH-4_p4fAMXPDWYouvoZdia88sWRsU', 
-        type:"GET",})
-  .done(function(response){
-    startLat = response.results[0].geometry.location.lat;
-    startLng = response.results[0].geometry.location.lng;
-  })
-  .fail(function(error){
-    console.log(error)
-  })
-  .then(function(){
-    $.ajax({url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + destAddress + ',+' + destCity + ',+' + destState + '&key=AIzaSyC8RAH-4_p4fAMXPDWYouvoZdia88sWRsU', 
-            type:"GET",})
-    .done(function(response){
-      destLat = response.results[0].geometry.location.lat;
-      destLng = response.results[0].geometry.location.lng;
+  $.ajax({
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + ',+' + city + ',+' + state + '&key=AIzaSyC8RAH-4_p4fAMXPDWYouvoZdia88sWRsU', 
+    type:"GET",  
     })
-    .then(function(){
-      $.ajax({url: 'https://api.lyft.com/v1/cost?start_lat=' +startLat + '&start_lng=' + startLng + "&end_lat=" + destLat + "&end_lng=" + destLng, 
-            type:"GET",
-            headers:{'Authorization': 'Bearer cCua1E9wIl6vB0YF61xLMi8DnUor7q4LyzjKwKclz4bIOeN6czq2YTSPos6t5Qgt2WRtpLdRYQz8fWalrvXyuUjkaFINNt3pzHkEpAyLSSaHBGcXcwlw2RM='}})
-      .done(function(response){
-        console.log("Below are the results coming back from Lyft: ");
-        parseLyftData(response);
-        apples(response)(response);
-      })
+    .done(function(response){
+      startLat = response.results[0].geometry.location.lat;
+      startLng = response.results[0].geometry.location.lng;
     })
     .fail(function(error){
       console.log(error)
     })
+    .then(function(){
+      $.ajax({
+        url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + destAddress + ',+' + destCity + ',+' + destState + '&key=AIzaSyC8RAH-4_p4fAMXPDWYouvoZdia88sWRsU', 
+        type:"GET",
+  
+      })
+      .done(function(response){
+        destLat = response.results[0].geometry.location.lat;
+        destLng = response.results[0].geometry.location.lng;
+      })
+      .then(function(){
+        $.ajax({
+          url: 'https://api.lyft.com/v1/cost?start_lat=' +startLat + '&start_lng=' + startLng + "&end_lat=" + destLat + "&end_lng=" + destLng, 
+          type:"GET",
+          headers:{'Authorization': 'Bearer cCua1E9wIl6vB0YF61xLMi8DnUor7q4LyzjKwKclz4bIOeN6czq2YTSPos6t5Qgt2WRtpLdRYQz8fWalrvXyuUjkaFINNt3pzHkEpAyLSSaHBGcXcwlw2RM='},
+    
+        })
+        .done(function(response){
+          console.log("Below are the results coming back from Lyft: ");
+          parseLyftData(response);
+          let lyftData = lyftLineChart(response);
+          let lyftDataSet = lyftData[0];
+          let lyftLabels = lyftData[1];
+          let lyftCostData = [lyftLabels, lyftDataSet];
+          console.log(lyftCostData);
+          $.ajax({
+            url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + ',+' + city + ',+' + state + '&key=AIzaSyC8RAH-4_p4fAMXPDWYouvoZdia88sWRsU', 
+            type:"GET",
+      
+          })
+            .done(function(response){
+              startLat = response.results[0].geometry.location.lat;
+              startLng = response.results[0].geometry.location.lng;
+            })
+              .fail(function(error){
+              console.log(error)
+            })
+              .then(function(){
+              $.ajax({
+                url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + destAddress + ',+' + destCity + ',+' + destState + '&key=AIzaSyC8RAH-4_p4fAMXPDWYouvoZdia88sWRsU', 
+                type:"GET",
+          
+              })
+            .done(function(response){
+              destLat = response.results[0].geometry.location.lat;
+              destLng = response.results[0].geometry.location.lng;
+            })
+            .then(function(){
+              $.ajax({
+              url: 'https://api.uber.com/v1.2/estimates/price?start_latitude=' + startLat + '&start_longitude=' + startLng + '&end_latitude=' + destLat + '&end_longitude=' + destLng, 
+              type: 'GET',
+              headers:{Authorization: 'Token YEEveIYDU-uU4BRcgORqnvoLRtrCtQDzc0yvbRVs'},
+        
+            })
+              .done(function(response){
+                console.log("Below are the results coming back from Uber: ")
+                console.log("Uberdata" + response);
+                console.log("lyftdata: " + lyftCostData);
+                let uberData = uberLineChart(response);
+                LineChartRender(lyftLabels, lyftDataSet, uberData);
+
+                
+              })
+            .fail(function(error){
+              console.log(error)
+            })
+            })
+          })
+      
+      })
+    })
   })
-//Lyft Ends
-//Uber Call
-  $.ajax({url: 'https://api.uber.com/v1.2/products?latitude=37.7752315&longitude=-122.418075', 
-          type: 'GET',
-          headers:{Authorization: 'Token YEEveIYDU-uU4BRcgORqnvoLRtrCtQDzc0yvbRVs'}})
-          .done(function(response){
-            console.log("Below are the results coming back from Uber: ")
-            console.log(response)
-          }).fail(function(error){
-            console.log(error)
-          });
-              }
+}
+
+
+
+
+
 $("#submit").on("click", submitInfo);
 $("#currentLocation").on("click", getCurrentLocation)
 
-function apples(response){
-  console.log("Got response in chart function");
-    return function chartTest(response) {
-        console.log(response);
-        let labels = [];
-        for(var i = 0; i < response.cost_estimates.length; i++) {
-            labels.push("Driver " + (i + 1));
-        }
-        console.log(labels);
-
-        let costData = [];
-        for(var i = 0; i < response.cost_estimates.length; i++) {
-            costData.push(response.cost_estimates[i].estimated_cost_cents_max);
-        }
-        console.log(costData);
-
-        const lineChart = document.getElementById('line-chart').getContext('2d');
-        const textLineChart = new Chart(lineChart, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: "Placeholder Data Set",
-                    fill: false,
-                    lineTenstion: 0,
-                    borderColor: 'rgb(255, 255, 255)',
-                    data: costData,
-                }
-                // {
-                //     label: "2nd Data Set",
-                //     fill: false,
-                //     backgroundColor: 'rgb(0, 255, 0)',
-                //     borderColor: 'rgb(0, 0, 255)',
-                //     data: [0, 5, 25, 25, 20, 30, 35],
-                // }       
-                ]}
-        });
-        console.log(textLineChart);
-    }
-}
